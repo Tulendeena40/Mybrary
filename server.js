9de6dcb0b888fed6323041c76c3c1537a2d00ccf
Tulendeena40 <crockliff@student.unimelb.edu.au>
2020-09-03 15:27:21 +1000

    Initial Setup

---
+++ server.js
@@ -0,0 +1,43 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+
+
+app.set('view engine', 'ejs')
+//Set where views are going to be coming from (path)
+app.set('views', __dirname + '/views')
+
+//Set where layouts are going to be coming from (path)
+app.set('layout', 'layouts/layout')
+// Tell express we want to use express layouts
+app.use(expressLayouts)
+
+// Tell express where our public files will be (files, sheets, images)
+app.use(express.static('public'))
+
+//import router
+app.use('/', indexRouter)
+
+// pull from an environment variable for when we deploy, development we choose port 3000
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('Server started on port 3000');
+})
+
+
+
